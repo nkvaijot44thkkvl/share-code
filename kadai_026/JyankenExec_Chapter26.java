65eaf91602c456b642a42b0d1ae17ac9b73a91c2
近藤　望 <li2xue3mei2@gmail.com>
2025-01-14 22:49:07 +0900

    Update JyankenExec_Chapter26.java

--- kadai_026/JyankenExec_Chapter26.java
+++ kadai_026/JyankenExec_Chapter26.java
@@ -3,6 +3,13 @@ package kadai_026;
 public class JyankenExec_Chapter26 {
 
 	public static void main(String[] args) {
+		//じゃんけんクラスのインスタンス作成
+		
+		//自分のじゃんけんの手を入力
+		
+		//対戦相手のじゃんけんがランダムで選ばれる
+		
+		//じゃんけんの結果を出力する
 		
 
 	}
