7df8947f400c3e863817a79e99d63a64eb2f8802
近藤　望 <li2xue3mei2@gmail.com>
2025-01-14 22:48:48 +0900

    Update Jyanken_Chapter26.java

--- kadai_026/Jyanken_Chapter26.java
+++ kadai_026/Jyanken_Chapter26.java
@@ -1,5 +1,21 @@
 package kadai_026;
 
-public class Jyanken_Chapter26 {
+import java.util.HashMap;
 
+public class Jyanken_Chapter26 {
+	HashMap<String,String> jyanken = new HashMap<String,String>;
+	
+	public Jyanken_Chapter26() {
+		jyanken.put("r", "グー");
+		jyanken.put("s", "チョキ");
+		jyanken.put("p", "パー");
+	}
+	
+	public void playGame(String deme) {
+		switch (deme) {
+		case "" -> {}
+		case "" -> {}
+		case "" -> {}
+		}
+	}
 }
